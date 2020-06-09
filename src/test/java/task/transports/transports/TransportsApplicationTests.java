4b76a8024f80b4c803a5c9a85c159161a56641c8
TeodorRussu <teodor_russu@yahoo.com>
2020-06-09 17:26:38 +0300

    first commit

---
+++ src/test/java/task/transports/transports/TransportsApplicationTests.java
@@ -0,0 +1,13 @@
+package task.transports.transports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
